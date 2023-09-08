9712a6011d691a89ef3c75901fdd129e97caf5f5
Raphael Rodrigues <noh4nsen@gmail.com>
2023-09-08 18:50:55 -0300

    add TODO check

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello")
+	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && strings.HasSuffix(info.Name(), ".go") {
+			if err := checkFile(path); err != nil {
+				fmt.Printf("Error in %s: %s\n", path, err)
+				os.Exit(1)
+			}
+		}
+		return nil
+	})
+}
+
+func checkFile(filename string) error {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	if strings.Contains(string(content), "TODO") {
+		return fmt.Errorf("found 'TODO' in %s", filename)
+	}
+
+	return nil
 }
