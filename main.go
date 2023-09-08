5e71d5de7a7972e0cc9351d07f5a2da98f74dcc4
Raphael Rodrigues <noh4nsen@gmail.com>
2023-09-08 19:50:13 -0300

    update to receive args

--- main.go
+++ main.go
@@ -8,18 +8,37 @@ import (
 )
 
 func main() {
-	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() && strings.HasSuffix(info.Name(), ".go") {
-			if err := checkFile(path); err != nil {
-				fmt.Printf("Error in %s: %s\n", path, err)
-				os.Exit(1)
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: test-pre-commit <file_or_directory>")
+		os.Exit(1)
+	}
+
+	target := os.Args[1]
+	fileInfo, err := os.Stat(target)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if fileInfo.IsDir() {
+		filepath.Walk(target, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
 			}
+			if !info.IsDir() && strings.HasSuffix(info.Name(), ".go") {
+				if err := checkFile(path); err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+			}
+			return nil
+		})
+	} else {
+		if err := checkFile(target); err != nil {
+			fmt.Printf("Error in %s: %s\n", target, err)
+			os.Exit(1)
 		}
-		return nil
-	})
+	}
 }
 
 func checkFile(filename string) error {
